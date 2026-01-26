48d4d8183745c88545a9cfdf5aeb560e466e90c2
aishwaryr <araj713@gmail.com>
2026-01-26 15:31:06 +0530

    add useState to select pizza type and size

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import Pizza from './Pizza';
 
 export default function Order() {
-  const pizzaType = 'pepperoni';
-  const pizzaSize = 'M';
+  // const pizzaType = 'pepperoni';
+  // const pizzaSize = 'M';
+
+  const [pizzaType, setPizzaType] = useState('pepperoni');
+  const [pizzaSize, setPizzaSize] = useState('M');
+
   return (
     <div className="order">
       <h2>Create Order</h2>
@@ -10,12 +15,17 @@ export default function Order() {
         <div>
           <div>
             <label htmlFor="pizza-type">Pizza Type</label>
-            <select name="pizza-type" value={pizzaType}>
+            <select
+              onChange={(e) => setPizzaType(e.target.value)}
+              name="pizza-type"
+              value={pizzaType}
+            >
               <option value="pepperoni">The Pepperoni Pizza</option>
               <option value="hawaiian">The Hawaiian Pizza</option>
               <option value="big_meat">The Big Meat Pizza</option>
             </select>
           </div>
+
           <div>
             <label htmlFor="pizza-size">Pizza Size</label>
             <div>
@@ -26,6 +36,7 @@ export default function Order() {
                   name="pizza-size"
                   value="S"
                   id="pizza-s"
+                  onChange={(e) => setPizzaSize(e.target.value)}
                 />
                 <label htmlFor="pizza-s">Small</label>
               </span>
@@ -36,6 +47,7 @@ export default function Order() {
                   name="pizza-size"
                   value="M"
                   id="pizza-m"
+                  onChange={(e) => setPizzaSize(e.target.value)}
                 />
                 <label htmlFor="pizza-m">Medium</label>
               </span>
@@ -46,11 +58,13 @@ export default function Order() {
                   name="pizza-size"
                   value="L"
                   id="pizza-l"
+                  onChange={(e) => setPizzaSize(e.target.value)}
                 />
                 <label htmlFor="pizza-l">Large</label>
               </span>
             </div>
           </div>
+
           <button type="submit">Add to Cart</button>
         </div>
         <div className="order-pizza">
